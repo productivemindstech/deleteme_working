72f2adf31acd71f8cf3bc686a6ba90177d05addd
ProductiveMindsTech <productivemindstech@gmail.com>
2019-09-13 09:19:58 +0530

    PHP working code for CI

---
+++ before_script.sh
@@ -0,0 +1,10 @@
+cd html
+echo "server starting..."
+pwd 
+python -m SimpleHTTPServer 3000 > /dev/null 2>&1 &
+sleep 5
+cd ..
+echo "Starting sauce connect"
+bin/sc -u pubinator -k 580f062b-af5a-4982-85dc-4619f66a2987 > /dev/null 2>&1 &
+sleep 30
+echo "after connecting sauce connect"
---
+++ tests/HelloWorldTest.php
@@ -0,0 +1,21 @@
+
+<?php
+  // Setup: $ php composer.phar require facebook/webdriver
+ 
+  require_once('vendor/autoload.php');
+  use Facebook\WebDriver\Remote\RemoteWebDriver;
+  use Facebook\WebDriver\WebDriverBy;
+ 
+  $web_driver = RemoteWebDriver::create(
+    "http://pubinator:580f062b-af5a-4982-85dc-4619f66a2987@@ondemand.saucelabs.com/wd/hub",
+    array("name"=>"PHP demo Test","platform"=>"Windows 10", "browserName"=>"chrome", "version"=>"76")
+  );
+  $web_driver->get("http://127.0.0.1:3000/hello.html");
+ 
+  /*
+    Test actions here...
+  */
+  echo $web_driver->getTitle();
+ 
+  $web_driver->quit();
+?>
